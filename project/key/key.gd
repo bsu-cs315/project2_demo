166702220c941aae3728f144857e30c5c9437acc
Paul Gestwicki <pvgestwicki@bsu.edu>
2023-09-21 13:44:39 -0400

    Play pickup sound by adding a player to the world
    This was the final form of a series of explorations, not necessarily
    the best way

--- project/key/key.gd
+++ project/key/key.gd
@@ -14,4 +14,13 @@ func _process(_delta):
 func _on_body_entered(body):
 	if body.is_in_group("aliens"):
 		body.keys += 1
+		
+		var audio_stream_player := AudioStreamPlayer.new()
+		audio_stream_player.stream = load("res://key/pickup_sound.wav")
+		get_parent().add_child(audio_stream_player)
+		audio_stream_player.play()
+		audio_stream_player.finished.connect(func():
+			audio_stream_player.queue_free()
+		)
+		
 		queue_free()

599c06036459e5ce763bd4b44b85879952accf27
Paul Gestwicki <pvgestwicki@bsu.edu>
2023-09-26 13:28:36 -0400

    Export the array of scene paths for random generation

--- project/world/world.gd
+++ project/world/world.gd
@@ -1,16 +1,20 @@
 extends Node2D
 
-const _OBJECT_TYPES := [
-	preload("res://key/key.tscn"),
-	preload("res://gem/gem.tscn"),
+@export var object_types : Array[String] = [
+	"res://key/key.tscn",
+	"res://gem/gem.tscn"
 ]
 
 @onready var _object_positions := $ObjectPositions
 @onready var _objects := $Objects
 
 func _ready():
+	if object_types.is_empty():
+		push_error("Uh oh, object types should not be empty.")
+	
 	for child in _object_positions.get_children():
-		var object : Node2D = _OBJECT_TYPES.pick_random().instantiate()
+		var scene_path : String = object_types.pick_random()
+		var object : Node2D = load(scene_path).instantiate()
 		object.global_position = child.global_position
 		_objects.add_child(object)
 

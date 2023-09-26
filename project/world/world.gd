92844f9e499233b836f883a961c8e5c24aa3b4ef
Paul Gestwicki <pvgestwicki@bsu.edu>
2023-09-26 13:12:08 -0400

    Generate gems or keys randomly in the world

--- project/world/world.gd
+++ project/world/world.gd
@@ -1,6 +1,19 @@
 extends Node2D
 
+const _OBJECT_TYPES := [
+	preload("res://key/key.tscn"),
+	preload("res://gem/gem.tscn"),
+]
+
+func _ready():
+	for child in $Objects.get_children():
+		var object : Node2D = _OBJECT_TYPES.pick_random().instantiate()
+		object.global_position = child.global_position
+		add_child(object)
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
 	$KeysLabel.text = "Keys: %d" % $Alien.keys
+
+
